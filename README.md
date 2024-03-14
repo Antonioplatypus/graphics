4d27820a568aeac2890cbfac0b8659d80124d521
Antonioplatypus <76115170+Antonioplatypus@users.noreply.github.com>
2024-03-14 19:00:41 +0200

    Add pong space invaders quiz

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ CREDITS:
 
 main page image : https://gamestate.com/blogs/news/the-evolution-of-video-game-graphics-from-8-bit-to-hd-and-vr
 Tennis for two images, text: https://en.wikipedia.org/wiki/Tennis_for_Two
+Pong : https://en.wikipedia.org/wiki/Pong
---
+++ quizscript.js
@@ -0,0 +1,140 @@
+const questions = [
+    {
+    question: "Which was the first game ever made?",
+    answers: [
+        {text: "Tennis for Two", correct:true},
+        {text: "Pong", correct:false},
+        {text: "Mario", correct:false},
+        {text: "Fortnite", correct:false},
+    ]
+    },
+    {
+    question: "Who manufactured Pong?",
+    answers: [
+        {text: "Microsoft", correct:false},
+        {text: "Sony", correct:false},
+        {text: "Atari", correct:true},
+        {text: "Microsoft", correct:false},
+    ]
+    },
+    {
+    question: "Space invaders was the first game to: ",
+    answers: [
+        {text: "have enemies", correct:false},
+        {text: "take place in space", correct:false},
+        {text: "save the player's score", correct:true},
+    ]
+    },
+    {
+    question: "Which was the first game ever made?",
+    answers: [
+        {text: "Tennis for Two", correct:true},
+        {text: "Pong", correct:false},
+        {text: "Mario", correct:false},
+        {text: "Fortnite", correct:false},
+    ]
+    },
+    {
+    question: "Which was the first game ever made?",
+    answers: [
+        {text: "Tennis for Two", correct:true},
+        {text: "Pong", correct:false},
+        {text: "Mario", correct:false},
+        {text: "Fortnite", correct:false},
+    ]
+    },
+    {
+    question: "Which was the first game ever made?",
+    answers: [
+        {text: "Tennis for Two", correct:true},
+        {text: "Pong", correct:false},
+        {text: "Mario", correct:false},
+        {text: "Fortnite", correct:false},
+    ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    resetState();
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("appbtn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex  < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
